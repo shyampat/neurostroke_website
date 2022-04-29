1c4ebd8654829396fbe2c1668fc07a5880635b02
Shyam Patel <shyampat@usc.edu>
2022-04-28 20:54:53 -0700

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-	// Define constants - constants cannot be changed after they have been declared.
-	define("DB_HOST", "303.itpwebdev.com");
-	define("DB_USER", "shyampat_db_user");
-	define("DB_PASS", "USCtrojan");
-	define("DB_NAME", "shyampat_final_project");
-?>
